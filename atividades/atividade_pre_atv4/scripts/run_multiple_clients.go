c594ee8d2f168fc44880099eda07f73244c8d61f
arthur brito <arthurmedeiros32@gmail.com>
2023-07-12 11:55:39 -0300

    feat: add script to run multiple clients

---
+++ atividades/atividade_pre_atv4/scripts/run_multiple_clients.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+const (
+	NumClients = 80
+)
+
+func main() {
+	for i := 0; i < NumClients; i++ {
+		cmd := exec.Command("go", "run", "../client/client.go")
+		err := cmd.Start()
+		if err != nil {
+			log.Fatalf("Failed to start client %d: %s", i, err)
+		}
+		log.Printf("Started client %d", i)
+	}
+}
