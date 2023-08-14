f42dd4d3c81aa2a08615a345ca91962ed81ea65c
Tomás Barros <tnpb@cin.ufpe.br>
2023-08-14 02:50:52 -0300

    fixed little bugs

--- atividades/atv4/udp/scripts/move_data.sh
+++ atividades/atv4/udp/scripts/move_data.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-
+ 
+# Directory paths
+SCRIPT_DIR="$(dirname "$0")"
+DATA_DIR="$SCRIPT_DIR/../data"
+SAMPLES_DIR="$SCRIPT_DIR/../samples/async"
+ 
 # Check if the argument is provided
 if [ -z "$1" ]; then
   echo "Please provide the data folder number as an argument."
   exit 1
 fi
-
+ 
 # Create the samples directory if it doesn't exist
-mkdir -p ../samples
-
+mkdir -p "$SAMPLES_DIR"
+ 
 # Create the specific data folder inside samples
-mkdir -p ../samples/data$1
-
+mkdir -p "$SAMPLES_DIR/data$1"
+ 
 # Check if there are any CSV files in the data folder
-if [ -z "$(ls -A ../data/*.csv 2>/dev/null)" ]; then
+if [ -z "$(ls -A "$DATA_DIR"/*.csv 2>/dev/null)" ]; then
   echo "No CSV files found in the data folder."
   exit 1
 fi
-
+ 
 # Move the CSV files to the respective data folder inside samples
-mv ../data/*.csv ../samples/data$1/
-
-echo "Files moved to samples/data$1/"
+mv "$DATA_DIR"/*.csv "$SAMPLES_DIR/data$1/"
+ 
+echo "Files moved to samples/async/data$1/"
